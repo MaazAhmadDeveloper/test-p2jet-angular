5e35c2095f177ea2c7b745b84ed31b7255809741
Maaz Ahmad <bragoname1125@gmail.com>
2024-09-24 19:42:27 +0500

    v1.2

--- src/app/denver-module/hero-image/hero-image.component.ts
+++ src/app/denver-module/hero-image/hero-image.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-hero-image',
   templateUrl: './hero-image.component.html',
   styleUrl: './hero-image.component.css'
-})
+}) 
 export class HeroImageComponent {
 
 }
